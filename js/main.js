be221bd252fd19a8a7f1fa3ca0f8ab2c96d1f42c
urpylka <urpylka@users.noreply.github.com>
2020-04-17 21:33:41 +0000

    Update pages on 2020-04-17 21:33:41

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let toggle = document.getElementById("scheme-toggle");
+  const toggle = document.getElementById("scheme-toggle");
+  const darkScheme = document.getElementById("dark-scheme");
+
+  const DARK = '(prefers-color-scheme: dark)';
+  const LIGHT = '(prefers-color-scheme: light)';
+
+  function detectColorScheme() {
+    // https://medium.com/free-code-camp/how-to-detect-a-users-preferred-color-scheme-in-javascript-ec8ee514f1ef
+    // https://ru.hexlet.io/blog/posts/novye-mediazaprosy-kotorye-izmenyat-vashi-predstavleniya-o-vozmozhnostyah-css
+    if(!window.matchMedia) {
+        return
+    }
+    function listener({matches, media}) {
+        if(!matches) { // Not matching anymore = not interesting
+            return
+        }
+        if(media === DARK) {
+            darkscheme(toggle, darkScheme);
+        } else if (media === LIGHT) {
+            lightscheme(toggle, darkScheme);
+        }
+    }
+    const mqDark = window.matchMedia(DARK)
+    mqDark.addListener(listener)
+    const mqLight = window.matchMedia(LIGHT)
+    mqLight.addListener(listener)
+  }
 
   let scheme = "light";
-  let savedScheme = localStorage.getItem("scheme");
-
-  let darkScheme = document.getElementById("dark-scheme");
-  // read initial scheme from html (and logic of hugo)
-  // but it didn't read if it set to light
-  let prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
+  // Check user's settings
+  let prefersDark = window.matchMedia(DARK).matches;
   if (prefersDark) {
     scheme = "dark";
   }
-
+  // Check saved settings
+  let savedScheme = localStorage.getItem("scheme");
   if(savedScheme) {
     scheme = savedScheme;
   }
 
-  if(scheme == "dark") {
+  if(scheme === "dark") {
     darkscheme(toggle, darkScheme);
   } else {
     lightscheme(toggle, darkScheme);
@@ -30,6 +52,9 @@ document.addEventListener("DOMContentLoaded", function(){
       lightscheme(toggle, darkScheme);
     }
   });
+
+  // Add user's windows listener
+  detectColorScheme();
 });
 
 function darkscheme(toggle, darkScheme) {
